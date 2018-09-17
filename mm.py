50e1f117822aab81f3cc84f31615b40b89fb861a
sakthiaravind <42993521+sakthiaravind@users.noreply.github.com>
2018-09-17 15:08:20 +0530

    Create mm.py

---
+++ mm.py
@@ -0,0 +1,8 @@
+l=(input())
+alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','s','r','t','u','v','w','x','y','z']
+if(l in alphabets):
+	
+	print("alphabet")
+else:
+	print("no")
+	
