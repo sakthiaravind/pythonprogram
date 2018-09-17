a5d1e5bfea3352089aa63d9538e18a840ca74251
sakthiaravind <42993521+sakthiaravind@users.noreply.github.com>
2018-09-17 14:58:28 +0530

    Create arav.py

---
+++ arav.py
@@ -0,0 +1,10 @@
+p=(input())
+consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+vowel=['a','e','i','o','u']
+if(p in vowel):
+	print("vowel")
+elif(p  in consonants):
+	print("Consonant")
+else:
+	print("invalid")
+	
